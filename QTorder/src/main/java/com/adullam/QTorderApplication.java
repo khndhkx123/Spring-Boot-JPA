deb3b133f8e46f4103d7f59bd04aea43b23e151e
khndhkx123 <khndhkx123@gmail.com>
2020-05-30 19:02:44 +0900

    ADD INIT : QTorder Project

---
+++ QTorder/src/main/java/com/adullam/QTorderApplication.java
@@ -0,0 +1,13 @@
+package com.adullam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QTorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QTorderApplication.class, args);
+	}
+
+}
