1efd98692af28a8e01670eaad05b1c667d796e17
khndhkx123@gmail.com <pows818239!>
2020-06-23 13:33:47 +0900

    Chapter 7 : 상속관계 매핑-분산테이블 전략

--- study/ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ study/ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -3,9 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn//DTYPE 의 추가(Album / Movie / Book), (name = "DIS_TYPE") 도 가능.
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
