8ad5dfbcb4d59638d628480159288de7649048c7
khndhkx123 <khndhkx123@gmail.com>
2020-06-24 11:44:08 +0900

    Chapter 8 : equals 를 이용한 값과 객체타입의 비교

--- study/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ study/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -30,8 +31,19 @@ public class Address {
         return zipcode;
     }
 
-    //한 멤버의 주소를 바꿨는데 모든 멤버의 주소가 바뀌는 문제가 발생할 수 있다.
-    //해결책으로는 불변객체가 있다.
-    //오직 생성자로서만 값을 설정할수 있는것이 불변객체
-    //Setter 를 private 로 설정하는 것도 불변객체의 방법중 하나.
+    //객체타입의 비교를 위해서 equals를 따로 정의해야 한다. Java에서 기본적으로 생성해 주는 것이 좋다.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
 }
