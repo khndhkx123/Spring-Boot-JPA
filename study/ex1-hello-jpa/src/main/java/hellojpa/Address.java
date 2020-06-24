0aadead5b24daf09267971bc87930cd97675d718
khndhkx123 <khndhkx123@gmail.com>
2020-06-24 11:33:53 +0900

    Chapter 8 : 불변객체

--- study/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ study/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -22,23 +22,16 @@ public class Address {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
     }
 
     public String getZipcode() {
         return zipcode;
     }
 
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+    //한 멤버의 주소를 바꿨는데 모든 멤버의 주소가 바뀌는 문제가 발생할 수 있다.
+    //해결책으로는 불변객체가 있다.
+    //오직 생성자로서만 값을 설정할수 있는것이 불변객체
+    //Setter 를 private 로 설정하는 것도 불변객체의 방법중 하나.
 }
