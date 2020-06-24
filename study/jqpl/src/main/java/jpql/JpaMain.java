7df6f945de4e2825044f2c949a2b41b62005bf79
khndhkx123 <khndhkx123@gmail.com>
2020-06-24 17:14:42 +0900

    파라미터 바인딩

--- study/jqpl/src/main/java/jpql/JpaMain.java
+++ study/jqpl/src/main/java/jpql/JpaMain.java
@@ -23,12 +23,10 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query1.getResultList();
-            Member result = query1.getSingleResult();
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
 
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
 
             tx.commit();
         } catch (Exception e){
