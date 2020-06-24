da0c37634445a6826ae526c57e5f32d56e82815a
khndhkx123 <khndhkx123@gmail.com>
2020-06-24 17:10:03 +0900

    TypedQuery, Query, getResultList, getSingleResult 사용법

--- study/jqpl/src/main/java/jpql/JpaMain.java
+++ study/jqpl/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -24,6 +22,13 @@ public class JpaMain {
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            List<Member> resultList = query1.getResultList();
+            Member result = query1.getSingleResult();
+
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
 
             tx.commit();
         } catch (Exception e){
