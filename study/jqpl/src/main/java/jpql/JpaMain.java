39e17c8113bf0ea104060344d7000e7d5ccb3bdb
khndhkx123 <khndhkx123@gmail.com>
2020-06-25 11:57:32 +0900

    JPQL : * 프로젝션 하는 방법 *

--- study/jqpl/src/main/java/jpql/JpaMain.java
+++ study/jqpl/src/main/java/jpql/JpaMain.java
@@ -23,9 +23,25 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
+            em.flush();
+            em.clear();
+
+            List<Team> result = em.createQuery("select m.team from Member m", Team.class)
+                    .getResultList();
+            // 엔티티 프로젝션 중 자동으로 join이 일어나는 경우
+
+            List<Address> result2 = em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+            //임베디드 타입 프로젝션
+
+            List<String> result3 = em.createQuery("select m.username from Member m", String.class)
+                    .getResultList();
+            //스칼라 타입 ! Int 도 됨 !!
+
+            List<MemberDTO> result4 = em.createQuery("select distinct new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            //***** 생성자가 반드시 있는 DTO 를 통해서 값을 가져오는 방법 *****
+
 
 
             tx.commit();
