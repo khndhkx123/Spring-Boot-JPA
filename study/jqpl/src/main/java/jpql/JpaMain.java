8dcf8b97581af680c3b99fc55cc199f3070386f2
khndhkx123 <khndhkx123@gmail.com>
2020-06-28 23:16:40 +0900

    Fetch Join 을 사용해야 하는 이유

--- study/jqpl/src/main/java/jpql/JpaMain.java
+++ study/jqpl/src/main/java/jpql/JpaMain.java
@@ -8,7 +8,7 @@ public class JpaMain {
     public static void main(String[] args) {
         /** PersistenceUnitName : persistence.xml : Line 6 **/
         /** Just One Entity Manager Factory For Project **/
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql");
 
         /** Create EntityManager **/
         /** Entity Manager for Each function(class) : Use -> Close **/
@@ -18,21 +18,44 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
+            String query = "select m from Member m";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-            //페이징기법 사용 : 0번째부터 시작해서 총 LIMIT 10 개를 가져온다.
 
-
+            for(Member i : result){
+                System.out.println("member = " + i.getUsername() + ", " + i.getTeam().getName());
+                //회원1, 팀A(SQL)
+                //회원2, 팀A(1차캐시)
+                //회원3, 팀B(SQL)
+                //현재 이 3개를 가져오기 위해 쿼리가 총 3번 나간다. 회원 100명의 경우, 쿼리 가 100번 나간다, N+1 / 1+N 쿼리라고도 한다. 이 문제를 해결하기 위해 fetch Join 을 사용한다.
+            }
 
             tx.commit();
         } catch (Exception e){
