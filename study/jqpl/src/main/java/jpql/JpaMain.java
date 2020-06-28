bf8ad62f5da4f53c679cd140fb941340eab1d2e2
khndhkx123 <khndhkx123@gmail.com>
2020-06-28 23:32:02 +0900

    Fetch Join 사용 방법

--- study/jqpl/src/main/java/jpql/JpaMain.java
+++ study/jqpl/src/main/java/jpql/JpaMain.java
@@ -45,16 +45,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m";
+            String query = "select m from Member m join fetch m.team";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
             for(Member i : result){
                 System.out.println("member = " + i.getUsername() + ", " + i.getTeam().getName());
-                //회원1, 팀A(SQL)
-                //회원2, 팀A(1차캐시)
-                //회원3, 팀B(SQL)
-                //현재 이 3개를 가져오기 위해 쿼리가 총 3번 나간다. 회원 100명의 경우, 쿼리 가 100번 나간다, N+1 / 1+N 쿼리라고도 한다. 이 문제를 해결하기 위해 fetch Join 을 사용한다.
+                //쿼리가 한번 나갈때 모두 다 가져오게 된다.
             }
 
             tx.commit();
