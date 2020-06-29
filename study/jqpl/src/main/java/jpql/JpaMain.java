5ce33be6a8c6c5d72e76853aa06271ab6ecd2b12
khndhkx123 <khndhkx123@gmail.com>
2020-06-29 14:35:11 +0900

    Named 쿼리 사용법 : 실무에선 Spring Data JPA의 @Query로 사용한다.

--- study/jqpl/src/main/java/jpql/JpaMain.java
+++ study/jqpl/src/main/java/jpql/JpaMain.java
@@ -45,18 +45,12 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select distinct t from Team t join fetch t.members";
-            //중복을 제거하려면 Distinct 를 추가하면된다. JPQL의 distinct는 SQL의 distinct와, 객체의 distinct를 제공한다.
-            //DB입장에서 보면 원칙상 위에 distinct 는 team.member 가 제거될수 없다 데이터가 완전중복되지 않기 때문.
-            //하지만 JPQL에서는 객체단위로도 distinct를 하기 때문에 가능한 일이 된다.
-            List<Team> result = em.createQuery(query, Team.class)
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1")
                     .getResultList();
 
-            for(Team team : result){
-                System.out.println("member = " + team.getName() + "| members = " + team.getMembers().size());
-                for(Member member : team.getMembers()){
-                    System.out.println("-> member = " + member.getId());
-                }
+            for(Member member : resultList){
+                System.out.println("member = " + member.getId());
             }
 
             tx.commit();
