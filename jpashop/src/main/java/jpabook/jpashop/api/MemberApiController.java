2ea94a33d9fd1a44e922d3bedcd7380dd3329ded
khndhkx123 <khndhkx123@gmail.com>
2020-06-30 21:01:02 +0900

    API개발 기본 - 회원조회 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -9,12 +10,47 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    /** Entity 를 가져오는 방식이기 때문에 불필요한 orders 까지 다 가져오게 되는 단점이 있다. **/
+    /** 이 방법은 또 Entity 에 @JsonIgnore 추가를 통해 반영할 수 있다. **/
+    /** 하지만 이 방법은 다양한 case 에서 매번 @JsonIgnore 를 군데군데 추가해야 하고, Entity 에 Logic이 추가되는 단점이 있다. **/
+    /** 또 Entity 안에서 name -> username 으로 교체하는 경우 API스펙(JSON) 도 바뀐 상태로 GET 한다는 단점이 있다. **/
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    /** 별도의 DTO 를 만들어 사용하면, 별도로 요구하는 Data 를 추가하거나 소거하기도 편하다. **/
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<MemberDTO> collect = memberService.findMembers().stream().map(m -> new MemberDTO(m.getName(),m.getAddress()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO{
+        private String name;
+        private Address address;
+    }
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
